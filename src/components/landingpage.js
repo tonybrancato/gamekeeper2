be20a14c0c1e1a01a8e35d8e8401713848f5b42f
Tony Brancato <tonybrancato1@gmail.com>
2018-03-29 00:36:13 -0700

    not currently in use

--- src/components/landingpage.js
+++ src/components/landingpage.js
@@ -1,25 +1,35 @@
 import React from 'react';
 import './landingpage.css';
+import GameCard from './GameCard';
 
-export default function LandingPage() {
-  return (
-    <div className="landing-page">
+export class Games extends React.Component {
+  render() {
+    const games = (this.props.games).map((game, index) => {
+      return (
+        <div className="landing-page">
 
-      {/* Info box containing explanation */}
-      <div className="gamekeeper-intro">
-      YeeHaw!
-      </div>
+        {/* Info box containing explanation */}
+        <div className="gamekeeper-intro">
+        <GameCard 
+          name="Splendor"
+  
+        />
+        </div>
+  
+        {/* Boxes displaying the two GameKeepers */}
+        <div className="gamekeeper-one">
+  
+        </div>
+        <div className="gamekeeper-two">
+        
+        </div>
+  
+        {/* Footer with portfolio, github, and LinkedIn links */}
+  
+        </div>
+      )
 
-      {/* Boxes displaying the two GameKeepers */}
-      <div className="gamekeeper-one">
+    })
 
-      </div>
-      <div className="gamekeeper-two">
-      
-      </div>
-
-      {/* Footer with portfolio, github, and LinkedIn links */}
-
-    </div>
-  );
+  }
 }
