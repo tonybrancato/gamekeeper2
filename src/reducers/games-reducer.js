90ca80f1813e4da054db60d288887fdae5cbe325
Tony Brancato <tonybrancato1@gmail.com>
2018-03-29 00:36:23 -0700

    init - mock data in use

---
+++ src/reducers/games-reducer.js
@@ -0,0 +1,20 @@
+// import moment from 'moment';
+
+const initialState = {
+  data: [
+    {
+      name: "Splendor"
+    },
+    {
+      name: "Star Wars Rebellion"
+    }
+  ],
+};
+
+
+
+export default function gamesReducer(state = initialState, action) {
+  
+  return state;
+
+}
