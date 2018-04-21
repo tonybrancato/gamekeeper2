dd2c0fbf68a23402f88bceba1c5956db87c4c457
Tony Brancato <tonybrancato1@gmail.com>
2018-04-21 11:08:00 -0400

    static props

--- src/components/GameCard.js
+++ src/components/GameCard.js
@@ -1,17 +1,17 @@
 import React from 'react';
+import './gamecard.css';
+import Rating from './rating'
 
 // pass props 
 export default class GameCard extends React.Component {
   render() {
     return (
-      <div classname="game-card">
-      {/* Props Needed:
-          NAME
-          GAME TYPE
-          PLAYS
-           */}
+      <div className="game-card">
         <h1 className="game-title">{this.props.name}</h1>
-
+        <h2>{this.props.type}</h2>
+        <h3>{this.props.type}</h3>
+        <h3>{this.props.players} players</h3>
+        <Rating />
       </div>
     )
   }
